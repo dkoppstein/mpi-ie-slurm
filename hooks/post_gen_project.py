2d33a337daac3f43af4982f7de0aca95f44b4a28
David Koppstein <koppstein@ie-freiburg.mpg.de>
2022-10-06 13:11:27 +0200

    deal with lists in config

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,9 +8,21 @@ with open("config.yaml", "r") as inh:
     config = yaml.safe_load(inh)
 new_config = {}
 
+
+def expandcheck(x):
+    if type(x) is str:
+        x = os.path.expandvars(x)
+    return x
+
+
 for k, v in config.items():
-    if type(v) is str:
-        v = os.path.expandvars(v)
+    v = expandcheck(v)
+    if type(v) is list:
+        new_list = []
+        for elem in v:
+            elem = expandcheck(elem)
+            new_list.append(elem)
+        v = new_list
     new_config[k] = v
 
 with open("config.yaml", "w") as outh:
